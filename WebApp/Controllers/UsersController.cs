d51e236cf35d5649d298dbcea6a742af83da63cf
Boris Belyy <hibarike@gmail.com>
2019-02-14 03:20:39 +0200

    Add

--- WebApp/Controllers/UsersController.cs
+++ WebApp/Controllers/UsersController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Runtime.Remoting.Contexts;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebApp.Models;
@@ -13,22 +16,16 @@ namespace WebApp.Controllers
 {
     public class UsersController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+        
         // GET: Users
         [Authorize(Roles = "admin")]
         public ActionResult Index()
         {
-            //IList<string> roles = new List<string> { "Роль не определена" };
-            //ApplicationUserManager userManager = HttpContext.GetOwinContext()
-            //                                        .GetUserManager<ApplicationUserManager>();
-            //ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
-
-            //if (user != null)
-            //    roles = userManager.GetRoles(user.Id);
-            //return View(roles);
-            var context = new ApplicationDbContext();
-            var allUsers = context.Users.ToList();
-            //ViewBag.llRoles = context.Roles.ToList();
-            IList<string> roles = new List<string> { "Роль не определена" };
+            
+            
+            var allUsers = db.Users.ToList();
+           // IList<string> roles = new List<string> { "Роль не определена" };
             ApplicationUserManager userManager = HttpContext.GetOwinContext()
                                                     .GetUserManager<ApplicationUserManager>();
             List<UserRole> UserRoles = new List<UserRole>();
@@ -48,8 +45,80 @@ namespace WebApp.Controllers
             return View(UserRoles);
             
         }
-        
-        
-       
+        // GET: Cities/Edit/5
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            ApplicationUserManager userManager = HttpContext.GetOwinContext()
+                                                       .GetUserManager<ApplicationUserManager>();
+
+            var allUsers = db.Users.ToList();
+            List<UserRole> UserRoles = new List<UserRole>();
+            foreach (var item in allUsers)
+            {
+                var _user = userManager.FindByEmail(User.Identity.Name);
+                UserRoles.Add(new UserRole
+                {
+                    user = _user,
+                    roles = userManager.GetRoles(_user.Id)
+                });
+            }
+           
+            return View(UserRoles);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,RolesId")] ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Cities/Delete/5
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ApplicationUser user = db.Users.Find(id);
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
     }
 }
--- WebApp/Models/AppDbInitializer.cs
+++ WebApp/Models/AppDbInitializer.cs
@@ -16,11 +16,14 @@ namespace RolesIdentityApp.Models
 
             // создаем две роли
             var role1 = new IdentityRole { Name = "admin" };
-            var role2 = new IdentityRole { Name = "user" };
-
+            var role2 = new IdentityRole { Name = "ContentManager" };
+            var role3 = new IdentityRole { Name = "Report 1" };
+            var role4 = new IdentityRole { Name = "Report 2" };
             // добавляем роли в бд
             roleManager.Create(role1);
             roleManager.Create(role2);
+            roleManager.Create(role3);
+            roleManager.Create(role4);
 
             // создаем пользователей
             var admin = new ApplicationUser { Email = "somemail@mail.ru", UserName = "somemail@mail.ru" };
